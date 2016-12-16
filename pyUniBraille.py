bb8938ac70b734cb107f51c78c771018110d37c5
Antonis Katzourakis <antonis.katzourakis@gmail.com>
2016-12-16 22:01:42 +0100

    Added a feature to get the translated Braille in Unicode representation.

--- pyUniBraille.py
+++ pyUniBraille.py
@@ -359,6 +359,30 @@ def translate(text, mainLanguage = None):
 
     return output
 
+def toUnicodeSymbols(brl, flatten=False):
+    """
+     Constructs the Unicode representation of a translated braille sentence.
+     If flatten=False, a list is returned in the same format as the input.
+     Otherwise, a string is returned with the translated Braille in Unicode.
+    """
+    retObj=[]
+    for wrd in brl:
+        retObj.append([])
+        for ch in wrd:
+            binary_repr = int(ch[::-1], 2)
+            hex_val = hex(binary_repr)[2:]
+            if len(hex_val) == 1: hex_val = "0" + hex_val
+
+            uni_code = "28{}".format(hex_val)
+            uni_code = unichr(int(uni_code, 16))
+            retObj[-1].append(uni_code)
+    
+    if flatten:
+        flattened_array = [i for j in retObj for i in j]
+        return " ".join(flattened_array)
+
+    return retObj
+
 def preprocess(text):
     """
     - Replaces all the available contractions.
