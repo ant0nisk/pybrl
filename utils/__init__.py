3ea1c5ada86118aab4b1c3a3d13dcc546228d38f
Jeremy Wright <wrightjmf@gmail.com>
2023-08-28 16:35:53 -0400

    Minor change to warning message when pdf_utils is not present

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 try:
     from . import pdf_utils
 except:
-    print("pdf_utils module not found, this funcationality will be disabled.")
+    print("pdf_utils module not found, PDF funcationality will be disabled.")
